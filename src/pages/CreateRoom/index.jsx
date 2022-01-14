f59a789f31cba2f2d9b1f32b0386b5dd206d6b10
João Pedro <jpmns9103@gmail.com>
2022-01-14 00:20:52 -0300

    criar funções da página de criação de sala

--- src/pages/CreateRoom/index.jsx
+++ src/pages/CreateRoom/index.jsx
@@ -1,31 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
 import ContainerMain from "../../components/ContainerMain";
 import Logo from "../../components/Logo";
 import FormInput from "../../components/FormInput";
 import FormButton from "../../components/FormButton";
 import Link from "../../components/Link";
+import LoadingGif from "../../components/LoadingGif/index";
 
 import Form from "../../styles/form";
 
 import { useAuth } from "../../providers/AuthProvider";
+import { useModal } from "../../providers/ModalProvider";
+
+import api from "../../services/api";
 
 const CreateRoom = () => {    
-
+    const [buttonChildren, setButtonChildren] = useState("Criar Sala");
+    const { handleShowModal } = useModal();
     const { handleLogout } = useAuth();
+    const navigate = useNavigate();
+    const handleLink = (link) => navigate(link);
+
+    const handleCreateRoom = async () => {
+      setButtonChildren(<LoadingGif />);
+  
+      const form = document.forms.createRoom;
+  
+      let { roomName } = form;
+  
+      if (!roomName.value) {
+        setButtonChildren("Criar Sala");
+        return handleShowModal("Preencha todos os campos");
+      }
+  
+      await api
+        .post(`/room`, {
+          name: roomName.value,
+        })
+        .then(({ data }) => {
+          roomName.value = "";
+          setButtonChildren("Criar Sala");
+          handleLink(`/room/${data.response}`);
+        })
+        .catch(({ response }) =>
+          response
+            ? handleShowModal(response.data.response)
+            : handleShowModal("Erro no Servidor")
+        );
+    };
+
+    useEffect(() => {
+      const handleShowRoom = async () => {
+        await api
+        .get(`/room-code`)
+        .then(({ data }) => {
+          if(data.response) handleLink(`/room/${data.response}`);
+        })
+        .catch(({ response }) =>
+          response
+            ? handleShowModal(response.data.response)
+            : handleShowModal("Erro no Servidor")
+        );
+      };
+      handleShowRoom();
+    });
 
     return ( 
         <>
             <ContainerMain>
-                <Form name="login">
+                <Form name="createRoom">
                     <Logo />
                     
                     <h2>Criar sala</h2>
 
                     <FormInput type="text" name="roomName" placeholder="Nome da Sala" />
 
-                    <FormButton>
-                        Criar Sala
+                    <FormButton onClick={() => handleCreateRoom()}>
+                        {buttonChildren}
                     </FormButton>
 
                     <Link link={"/enter-room"}>
